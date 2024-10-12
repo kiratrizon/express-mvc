8fe64e53e99642efe27703dfe9a4a0653a6da7c9
fdc-genesis <genesistroy.fdc@gmail.com>
2024-10-12 18:50:42 +0800

    wew

--- env.js
+++ env.js
@@ -1,3 +1,3 @@
-const dbconfig = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/express-app";
+        const dbconfig = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/express-app";
 
 module.exports = {dbconfig: dbconfig};
