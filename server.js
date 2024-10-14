119108bef543b471ae73b3a005d50aedb9e2bb34
kyuuukira <genesistroy.fdc@gmail.com>
2024-10-14 09:22:09 +0800

    deploy

--- server.js
+++ server.js
@@ -17,6 +17,8 @@ app.use((req, res) => {
     }
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+module.exports = app;
+
+// app.listen(PORT, () => {
+//     console.log(`Server is running on port ${PORT}`);
+// });
